5d72018da76b9abdfc37f5788b1077c6b6e835e3
swapnilRoyWeb <swapnilroyweb@gmail.com>
2023-05-28 23:22:05 +0600

    second banner added:

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Category from '../Category/Category';
+import SecondBanner from '../SecondBanner/SecondBanner';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <Category></Category>
+            <SecondBanner></SecondBanner>
         </div>
     );
 };
---
+++ src/Pages/Home/SecondBanner/SecondBanner.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import chefService from '../../../assets/home/chef-service.jpg'
+
+const SecondBanner = () => {
+    return (
+        <div className="hero min-h-screen my-10" style={{ backgroundImage: `url(${chefService})` }}>
+            <div className="hero-overlay bg-opacity-60"></div>
+            <div className="hero-content text-center bg-white px-52 py-32 border-red-500 border-8 rounded-3xl">
+                <div className="max-w-md">
+                    <h1 className="mb-5 text-5xl font-bold">Bistro Boss</h1>
+                    <p className="mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, libero accusamus laborum <br /> deserunt ratione dolor officiis praesentium! Deserunt magni aperiam dolor eius dolore at, nihil iusto <br /> ducimus incidunt quibusdam nemo.</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SecondBanner;
