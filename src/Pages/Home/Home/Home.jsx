34232cf0183d6c360e8117437d50db862454ac2b
swapnilRoyWeb <swapnilroyweb@gmail.com>
2023-05-30 09:19:03 +0600

    popular menu section added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Category from '../Category/Category';
+import PopularMenu from '../PopularMenu/PopularMenu';
 import SecondBanner from '../SecondBanner/SecondBanner';
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner></Banner>
             <Category></Category>
             <SecondBanner></SecondBanner>
+            <PopularMenu></PopularMenu>
         </div>
     );
 };
---
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -0,0 +1,33 @@
+import { data } from 'autoprefixer';
+import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+import MenuItem from '../../Shared/MenuItem/MenuItem';
+
+const PopularMenu = () => {
+
+    const [menu, setMenu] = useState([]);
+
+    useEffect(() => {
+        fetch('menu.json')
+        .then(res => res.json())
+        .then(data => {
+            const popularItems = data.filter(item => item.category === 'popular');
+            setMenu(popularItems)})
+    }, [])
+
+    return (
+       <section className='my-10 flex flex-col justify-center items-center'>
+        <SectionTitle subHeading={"Check it out"} heading={"From Our Menu"}></SectionTitle>
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-10'>
+            {
+                menu.map(item => <MenuItem key={item._id} item={item}></MenuItem>)
+            }
+        </div>
+        <button className='border-b-4 border-black mt-10 rounded-lg py-1 px-5 uppercase'>View Full Menu</button>
+       </section>
+    );
+};
+
+export default PopularMenu;
