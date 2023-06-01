bb6bc9dfe4ad098bebfb05bd6c0ec175549e47c8
swapnilRoyWeb <swapnilroyweb@gmail.com>
2023-06-02 00:32:13 +0600

    menu page and some section added in that page

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
+import Cover from '../../Shared/Cover/Cover';
+import menuImg from '../../../assets/menu/banner3.jpg'
+import PopularMenu from '../../Home/PopularMenu/PopularMenu';
 
 const Menu = () => {
     return (
@@ -7,7 +10,12 @@ const Menu = () => {
             <Helmet>
                 <title>Bistro Boss | Menu</title>
             </Helmet>
-            <h2>Menu</h2>
+            <Cover bgImg={menuImg} title="Our Menu"></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover bgImg={menuImg} title="Our Menu"></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover bgImg={menuImg} title="Our Menu"></Cover>
+            <PopularMenu></PopularMenu>
         </div>
     );
 };
